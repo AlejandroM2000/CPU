921c3d082c4815af139f819030d39351c440a71c
AlejandroM2000 <amalanch@ucsd.edu>
2022-02-05 23:19:59 -0800

    finished alu and added reg_file code

--- ALU.sv
+++ ALU.sv
@@ -1,21 +1,24 @@
-module ALU #(parameter W=8, OP=3) (
-    input logic      [W-1:0]InputA,
-                            InputB,
+module ALU #(parameter W=2, OP=3) (
+    input logic      [W-1:0]A,
+                            B,
     input logic     [Ops-1:0] OP,
     input logic
-    output logic    [W-1:0] out     
+    output logic    [W-1:0] out
+    output logic            Zero,
+                            Sign     
 
 
 );
     always_comb begin
         case(OP)
-            3'b000: out = InputA + InputB;
-            3'b001: out = 
-            3'b010: out =
-            3'b011: out =
-            3'b100: out = 
-            3'b101: out =
-            3'b110: out =
-            3'b111: out =
+            3'b000: out = A + B; // add 
+            3'b001: out = {1'b0,A[W-1:1]}; //shift right
+            3'b010: out = {A[W-2:0], 1'b0}; //shift left
+            3'b011: out = A ^ B;  //XOR
+        endcase
+    end
+            
 
+    assign Zero = !out;
+    assign Sign = out[0];
 endmodule
