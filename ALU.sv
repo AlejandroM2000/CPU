0dc2991399dc4a43ac733e0db15d9a8f67280ac6
AlejandroM2000 <amalanch@ucsd.edu>
2022-02-08 23:35:04 -0800

    added alu_tb

--- ALU.sv
+++ ALU.sv
@@ -1,8 +1,8 @@
-module ALU #(parameter W=2, OP=3) (
+module ALU #(parameter W=8, OP=3) (
     input logic      [W-1:0]A,
                             B,
     input logic     [Ops-1:0] OP,
-    input logic
+    // input logic
     output logic    [W-1:0] out
     output logic            Zero,
                             Sign     
@@ -12,8 +12,8 @@ module ALU #(parameter W=2, OP=3) (
     always_comb begin
         case(OP)
             3'b000: out = A + B; // add 
-            3'b001: out = {1'b0,A[W-1:1]}; //shift right
-            3'b010: out = {A[W-2:0], 1'b0}; //shift left
+            3'b001: out = {0,A[W-1:W-B]}; //shift right
+            3'b010: out = {A[W-B:0],0}; //shift left
             3'b011: out = A ^ B;  //XOR
         endcase
     end
---
+++ ALU_tb.sv
@@ -0,0 +1,66 @@
+module ALU_tb;
+
+timeunit  1ns/1ps;
+
+int width = 8;
+int op = 3;
+
+logic [width-1:0]A,
+                 B;
+logic [op-1:0]OP;
+logic [width-1:0] out;
+logic             Zero,
+                  Sign;    
+ALU uut(
+    .A(A),
+    .B(B),
+    .out(out),
+    .Zero(Zero),
+    .Sign(Sign)
+);
+
+initial begin
+
+ INPUTA = 1;
+ INPUTB = 1;
+ OP= 'b000; // ADD
+ test_alu_func; // void function call
+ #5;
+
+
+ INPUTA = 4;
+ INPUTB = 2;
+ OP= 'b001; // shift right
+ test_alu_func; // void function call
+ #5;
+
+
+INPUTA = 1;
+ INPUTB = 2;
+ OP= 'b010; // shift right
+ test_alu_func; // void function call
+ #5;
+
+ INPUTA = 5;
+ INPUTB = 5;
+ OP= 'b011; // XOR
+ test_alu_func; // void function call
+ #5;
+ end
+
+ task test_alu_func;
+ begin
+   case (op)
+  0: expected = INPUTA + INPUTB;  // ADD 
+  1: expected = {0,INPUTA[W-1:W-INPUTB];  // LSH
+  2: expected = {INPUTA[W-INPUTB:0],0};  // RSH
+  3: expected = INPUTA ^ INPUTB;  // XOR
+   endcase
+   #1; if(expected == OUT)
+  begin
+   $display("%t YAY!! inputs = %h %h, opcode = %b, Zero %b",$time, INPUTA,INPUTB,op, Zero);
+  end
+     else begin $display("%t FAIL! inputs = %h %h, opcode = %b, zero %b",$time, INPUTA,INPUTB,op, Zero);end
+
+ end
+ endtask
