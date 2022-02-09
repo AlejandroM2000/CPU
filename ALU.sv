d1c583fbae94a8683f93e5d6ee4fb90722c52d35
Luke <lmenezes@ucsd.edu>
2022-02-09 10:46:47 -0800

    temp fix for ALU

--- ALU.sv
+++ ALU.sv
@@ -11,9 +11,12 @@ module ALU #(parameter W=8, Ops=3) (
     always_comb begin
         case(OP)
             3'b000: out = A + B; // add 
-            3'b001: out = {0,A[W-1:W-B]}; //shift right
-            3'b010: out = {A[W-B:0],0}; //shift left
+            //3'b001: out = {0,A[W-1:W-B]}; //shift right
+				3'b001: out = A >> 1;
+            //3'b010: out = {A[W-B:0],0}; //shift left
+				3'b010: out = A << 1;
             3'b011: out = A ^ B;  //XOR
+				default: out = 0;
         endcase
     end
             
