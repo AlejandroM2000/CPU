42157ab6687dc27a1cf070768cab9c41d6d5af7e
AlejandroM2000 <amalanch@ucsd.edu>
2022-02-22 12:06:49 -0800

    fixed ALU

--- ALU.sv
+++ ALU.sv
@@ -11,8 +11,8 @@ module ALU #(parameter W=8, Ops=3) (
     always_comb begin
         case(OP)
             3'b000: out = A + B; // add 
-            3'b001: out = {0,A[W-1:W-B]}; //shift right
-            3'b010: out = {A[W-B:0],0}; //shift left
+            3'b001: out = A >> B; //shift right
+            3'b010: out = A << B; //shift left
             3'b011: out = A ^ B;  //XOR
         endcase
     end
