a5a8d72d802f9a17bf5bc6496546809c9a299383
AlejandroM2000 <amalanch@ucsd.edu>
2022-03-13 20:45:02 -0700

    XOR Change

--- ALU.sv
+++ ALU.sv
@@ -8,12 +8,13 @@ module ALU #(parameter W=8, Ops=3) (
 
 
 );
+
     always_comb begin
         case(OP)
             3'b000: out = A + B; // add 
             3'b001: out = A >> B; //shift right
             3'b010: out = A << B; //shift left
-            3'b011: out = A ^ B;  //XOR
+            3'b011: out = ^A;  //XOR
 		    default: out = 0;
         endcase
     end
